586780485df0ae5091a99a57610a254018e007cc
animesyon <121491211+animesyon@users.noreply.github.com>
2024-06-19 08:23:18 +0800

    initial commit

---
+++ 3dsReader/Resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 3dsreader.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_MY3DSREADER_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_MY3DSREADER			107
+#define IDI_SMALL				108
+#define IDC_MY3DSREADER			109
+
+#define IDI_ICON_1				151
+#define IDI_ICON_2				152
+#define IDI_ICON_3				153
+#define IDI_ICON_4				154
+
+#define IDM_OPEN				111
+
+#define IDC_EDIT1				1001
+#define IDC_EDIT2				1002
+#define IDC_TREE1				1003
+#define IDC_LIST1				1004
+
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
